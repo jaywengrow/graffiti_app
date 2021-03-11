dbb4f25f490d9a292bc230a53510003b7ecfd954
Jay Wengrow <jaywngrw@gmail.com>
2021-03-10 21:14:00 -0600

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <button v-on:click="getGraffiti()">Get Data</button>
+    <line-chart :chart-data="dataCollection"></line-chart>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import LineChart from '../components/LineChart.js'
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld
-  }
+      LineChart
+  },
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      wards: [],
+      dataCollection: null
+    };
+  },
+  created: function() {},
+  mounted: function() {
+    this.getGraffiti()
+  },
+  methods: {
+    getGraffiti: function() {
+      axios.get("https://data.cityofchicago.org/resource/hec5-y4x5.json").then(response => {
+        this.groupIntoWards(response.data);
+        this.fillChartData();
+      })
+    },
+    wardLabels: function() {
+      let labels = [];
+      for(let i = 1; i <= 50; i++) {
+        labels.push("Ward " + i);
+      }
+      return labels;
+    },
+    groupIntoWards: function(graffitiCalls) {
+      for(const graffitiCall of graffitiCalls) {
+        let ward = parseInt(graffitiCall.ward);
+        if(this.wards[ward]) {
+          this.wards[ward] += 1;
+        } else {
+          this.wards[ward] = 1
+        }
+      }
+    },
+    fillChartData: function() {
+      this.dataCollection = {
+          labels: this.wardLabels(),
+          datasets: [
+            {
+              label: '# of Graffiti Calls',
+              backgroundColor: '#f87979',
+              data: this.wards
+            }
+          ]
+        }
+    }
+  },
+  
 };
 </script>
